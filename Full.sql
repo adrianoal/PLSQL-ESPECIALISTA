50a45dc1dcfa1cd7c95b5736f33e76ccb4ef050a
Adriano Lima <adriano.lima85@yahoo.com.br>
2020-08-27 12:08:29 -0300

    10.Definindo Identificadores - Constantes

--- Full.sql
+++ Full.sql
@@ -206,10 +206,56 @@ END;
 ---------------------------------------------------------------------------------------------------			
 10.Definindo Identificadores - Constantes
 
+--
+-- Seção 6 - Declaração de Identificadores - Variáveis e Constantes
+--
+-- Aula 2 - Definindo Identificadores – Constantes
+--
+-- 
+
+-- Declarando Constantes
+
+-- Declarando uma Constante Tipo NUMBER
+
+SET SERVEROUTPUT ON
+DECLARE
+  vPi    CONSTANT NUMBER(38,15) := 3.141592653589793;
+  
+BEGIN
+  DBMS_OUTPUT.PUT_LINE('Pi = ' ||   vPi);
+
+END;
+-- Obs: O valor de constantes não são muito usados em abientes corporativos, é mais para sistemas
+     -- ciêntíficos, conforme o exemplo acima que guarda o valor de PI.
 
 
 
+-- Declarando Constante Tipo CHAR e VARCHAR2
 
+SET SERVEROUTPUT ON
+DECLARE
+  vCaracterTamFixo     CONSTANT CHAR(2) := 'RS';
+  vCaracterTamVariavel CONSTANT VARCHAR2(100) := 'Porto Alegre, RS';
+BEGIN
+  DBMS_OUTPUT.PUT_LINE('String Caracteres Tam Fixo = ' || vCaracterTamFixo );
+  DBMS_OUTPUT.PUT_LINE('String Caracteres Tam Variável = ' || vCaracterTamVariavel );
+  
+END;
+
+-- Declarando variáveis Tipo DATE
+
+SET SERVEROUTPUT ON
+DECLARE
+  vData1     DATE := '29/04/20';
+  vData2     DATE := '29/04/2020';
+BEGIN
+  DBMS_OUTPUT.PUT_LINE('Data1 = ' || vData1 );
+  DBMS_OUTPUT.PUT_LINE('Data2 = ' || vData2 );
+END;
+
+---------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------			
+11.Tipos de Dados no PL/SQL Datatypes
 
 
 
