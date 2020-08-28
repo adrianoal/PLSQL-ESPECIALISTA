01cadb480f0c7577d6bf6306477398cdbf60e67c
Adriano Lima <adriano.lima85@yahoo.com.br>
2020-08-28 13:58:32 -0300

    Seção 7:PL/SQL Fundamentos - 13.Sintaxe e Diretrizes de um bloco PL/SQL

--- Full.sql
+++ Full.sql
@@ -367,10 +367,47 @@ END;
 
 ---------------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------			
+Seção 7:PL/SQL Fundamentos - Sintaxe e Diretrizes de um Bloco PL/SQL
+
+13.Sintaxe e Diretrizes de um bloco PL/SQL
 
 
+ DIRETRIZES DE UM BLOCO PL/SQL:
+ ------------------------------
+ 
+ * Unidade Léxicas (identificadores ou literais - palavras de um comando) podem ser separadas por um ou mais 
+   espaços. 
 
 
+--
+-- Seção 7 - Sintaxe e Diretrizes de um Bloco PL/SQL
+--
+-- Aula 1 - Sintaxe e Diretrizes de um Bloco PL/SQL
+--
+
+-- Comentando o Código
+
+SET SERVEROUTPUT ON
+DECLARE
+  vNumero1  NUMBER(13,2);  -- Declaração de variável para o Primeiro número
+  vNumero2  NUMBER(13,2);  -- Declaração de variável para o Segundo número
+  vMedia    NUMBER(13,2);  -- Declaração de variável para receber a Media calculada
+BEGIN
+  /* Cálculo do valor da média entre dois números
+     Autor: Emílio Scudero
+     Data: 05/05/2020 */
+	 
+  vNumero1  :=  8000;
+  vNumero2  :=  4000;
+  vmedia    := (vNumero1 + vNumero2) / 2;           -- Cálculo da Media entre os dois números  
+  DBMS_OUTPUT.PUT_LINE('vnumero1 = ' || vNumero1);  -- Impressão do Primeiro Número
+  DBMS_OUTPUT.PUT_LINE('vnumero2 = ' || vNumero2);  -- Impressão do Segundo Número
+  DBMS_OUTPUT.PUT_LINE('Media = ' || vMedia);     	-- Impressão da Média calculada 
+END;
+
+---------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------			
+14.Utilizando Funções no bloco PL/SQL
 
 
 
