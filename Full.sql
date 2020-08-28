fdbd7cb3bb294e501ac470547a21d91f63472a9f
Adriano Lima <adriano.lima85@yahoo.com.br>
2020-08-28 17:08:57 -0300

    20.Utilizando o comando UPDATE no PL/SQL

--- Full.sql
+++ Full.sql
@@ -666,9 +666,54 @@ END;
 ---------------------------------------------------------------------------------------------------			
 19.Utilizando o comando INSERT no PL/SQL
 
+--
+-- Seção 8 - Utilizando comandos SQL no PL/SQL
+--
+-- Aula 3 - Utilizando o comando INSERT no PL/SQL
+--
+
+-- Utilizando o comando INSERT no PL/SQL
+
+SET SERVEROUTPUT ON
+DECLARE
+   --vfirst_name  employees.first_name%type;
+   --vlast_name   employees.last_name%type;
+   --vsalary      employees.salary%type;
+BEGIN
+   INSERT INTO employees 
+   (employee_id, first_name, last_name, email, phone_number, hire_date, job_id, salary, commission_pct, manager_id, department_id)
+    VALUES 
+    (employees_seq.nextval, 'Kobe', 'Bryant', 'KBRYANT', '515.123.45568', SYSDATE, 'IT_PROG', 15000, 0.4, 103, 60);
+   COMMIT;  
+END;
+
+---------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------			
+20.Utilizando o comando UPDATE no PL/SQL;
 
 
+--
+-- Seção 8 - Utilizando comandos SQL no PL/SQL
+--
+-- Aula 4 - Utilizando o comando UPDATE no PL/SQL
+--
 
+-- Utilizando o comando UPDATE no PL/SQL
+
+
+SET SERVEROUTPUT ON
+DECLARE
+   vEmployee_id    employees.employee_id%type := 150;
+   vPercentual     NUMBER(3) := 10;
+BEGIN
+   UPDATE employees 
+   SET    salary = salary * (1 + vPercentual / 100)
+   WHERE  employee_id =  vEmployee_id;
+   COMMIT;  
+END;
+
+---------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------			
 
 
 
