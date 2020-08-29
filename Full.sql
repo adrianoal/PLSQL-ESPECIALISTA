29b75d519109e9c653832da741958cb8031999ee
Adriano Lima <adriano.lima85@yahoo.com.br>
2020-08-29 15:27:08 -0300

    25.Utilizando o comando IF

--- Full.sql
+++ Full.sql
@@ -947,10 +947,80 @@ END;
 
 ---------------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------			
-Secao 9:PL/SQL Fundamentos -24.Operadores PL/SQL e Regra de precedencia
+25.Utilizando o comando IF
 
 
+--
+-- Seção 9 - Estruturas de Controle 
+--
+-- Aula 2 - Utilizando o comando IF
+--
 
+-- Utilizando o comando IF
+
+SET SERVEROUTPUT ON
+ACCEPT pdepartment_id PROMPT 'Digite o Id do departamento: ' 
+-- ACCEPT -- Comando do SQL*Plus, esse comando deve ser criado fora do bloco. ACCEPT define uma variavel.
+-- PROMPT --> Para exibir uma frase.
+DECLARE
+   vpercentual     NUMBER(3);
+   vDepartment_id  employees.employee_id%type := &pdepartment_id; -- Variavel de substituicao prefixa com & 
+BEGIN
+   IF  vDepartment_id  =  80 
+   THEN 
+       vpercentual := 10; -- Sales
+   ELSE 
+       IF  vDepartment_id  =  20 
+       THEN 
+           vpercentual := 15; -- Marketing
+       ELSE
+           IF  vDepartment_id  =  60 
+           THEN 
+               vpercentual := 20; -- IT
+           ELSE 
+               vpercentual := 5;
+           END IF;
+       END IF;
+  END IF;
+  
+  DBMS_OUTPUT.PUT_LINE('Id do Departamento: ' || vDepartment_id);   
+  DBMS_OUTPUT.PUT_LINE('Percentual: ' || vpercentual );   
+  
+  UPDATE employees 
+  SET    salary = salary * (1 + vpercentual / 100)
+  WHERE department_id =  vDepartment_id;
+  COMMIT;
+END;
+
+-- Comando IF  com ELSIF
+
+SET SERVEROUTPUT ON
+ACCEPT pdepartment_id PROMPT 'Digite o Id do departamento: '
+DECLARE
+   vpercentual     NUMBER(3);
+   vDepartment_id  employees.employee_id%type := &pdepartment_id;
+BEGIN
+   IF  vDepartment_id  =  80 
+   THEN 
+       vpercentual := 10; -- Sales
+   ELSIF vDepartment_id  =  20 
+   THEN 
+       vpercentual := 15; -- Marketing
+   ELSIF vDepartment_id  =  60 
+   THEN 
+       vpercentual := 20; -- IT
+   ELSE
+       vpercentual := 5;
+   END IF;
+  
+  DBMS_OUTPUT.PUT_LINE('Id do Departamento: ' || vDepartment_id);   
+  DBMS_OUTPUT.PUT_LINE('percentual: ' || vpercentual );   
+  
+  UPDATE employees 
+  SET    salary = salary * (1 + vpercentual / 100)
+  WHERE department_id =  vDepartment_id;
+  COMMIT;
+END;
 
 
 
