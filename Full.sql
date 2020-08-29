0b1d0603e4b187e9b9370a28ee0046cdd89b342e
Adriano Lima <adriano.lima85@yahoo.com.br>
2020-08-29 15:39:10 -0300

    26.tilizando o comando CASE

--- Full.sql
+++ Full.sql
@@ -949,7 +949,16 @@ END;
 ---------------------------------------------------------------------------------------------------			
 25.Utilizando o comando IF
 
+ -- NULOS EM EXPRESSÕES DE COMPARAÇÃO:
+ -------------------------------------
 
+ * Qualquer expressão que possui um valor nulo, retorna NULL(Cuidado)
+
+ * Qualquer expressão q possui um valor nulo retorna o booleano NULL
+ 
+ * Para comparar com o valor NULL deve ser utilizado o operador IS NULL ou IS NOT NULL ou 
+   tratar o valor NULL utilizando as funções NVL ou NVL2
+ 
 --
 -- Seção 9 - Estruturas de Controle 
 --
@@ -1021,6 +1030,94 @@ BEGIN
   WHERE department_id =  vDepartment_id;
   COMMIT;
 END;
+
+
+---------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------			
+26.tilizando o comando CASE 
+
+--
+-- Seção 9 - Estruturas de Controle 
+--
+-- Aula 3 - Utilizando o comando CASE
+--
+
+-- Utilizando o comando CASE
+
+SET SERVEROUTPUT ON
+ACCEPT pdepartment_id PROMPT 'Digite o Id do departamento: '
+DECLARE
+   vpercentual     NUMBER(3);
+   vDepartment_id  employees.employee_id%type := &pdepartment_id;
+BEGIN
+   CASE  vDepartment_id 
+   WHEN  80 
+   THEN 
+        vpercentual := 10; -- Sales
+   WHEN  20 
+   THEN 
+         vpercentual := 15; -- Marketing
+   WHEN  60 
+   THEN 
+         vpercentual := 20; -- IT
+   ELSE 
+         vpercentual := 5;
+   END CASE;
+   DBMS_OUTPUT.PUT_LINE('Id do Departamento: ' || vDepartment_id);   
+   DBMS_OUTPUT.PUT_LINE('percentual: ' || vPercentual );   
+   UPDATE employees 
+   SET salary = salary * (1 + vpercentual / 100)
+   WHERE department_id =  &pdepartment_id;
+   COMMIT; 
+ END;
+ 
+ -- Utilizando o comando CASE 2º opção
+ 
+SET SERVEROUTPUT ON
+ACCEPT pdepartment_id PROMPT 'Digite o Id do departamento: '
+DECLARE
+   vpercentual     NUMBER(3);
+   vDepartment_id  employees.employee_id%type := &pdepartment_id;
+BEGIN
+   CASE  
+   WHEN  vDepartment_id = 80 
+   THEN 
+        vpercentual := 10; -- Sales
+   WHEN  vDepartment_id = 20 
+   THEN 
+         vpercentual := 15; -- Marketing
+   WHEN  vDepartment_id = 60 
+   THEN 
+         vpercentual := 20; -- IT
+   ELSE 
+         vpercentual := 5;
+   END CASE;
+   DBMS_OUTPUT.PUT_LINE('Id do Departamento: ' || vDepartment_id);   
+   DBMS_OUTPUT.PUT_LINE('percentual: ' || vpercentual );   
+   UPDATE employees 
+   SET salary = salary * (1 + vpercentual / 100)
+   WHERE department_id =  &pdepartment_id;
+   --COMMIT; 
+ END;
+ 
+---------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------			
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
