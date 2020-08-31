c286e42e414ea7ada47424b9bcee27488aaaf787
Adriano Lima <adriano.lima85@yahoo.com.br>
2020-08-31 10:59:21 -0300

    28.FOR LOOP

--- Full.sql
+++ Full.sql
@@ -1136,17 +1136,40 @@ DECLARE
   vNumero  NUMBER(38) := 1;
   vLimite  NUMBER(38) := &pLimite;
 BEGIN
--- Imprimindo números de 1 até o limite
-LOOP 
-  DBMS_OUTPUT.PUT_LINE('Número = ' || to_char(vNumero));
-  EXIT WHEN vNumero = vLimite;
-  vNumero := vNumero + 1;
-END LOOP;
+	-- Imprimindo números de 1 até o limite
+	LOOP 
+	  DBMS_OUTPUT.PUT_LINE('Número = ' || to_char(vNumero));
+	  EXIT WHEN vNumero = vLimite;
+	  vNumero := vNumero + 1;
+	END LOOP;
 END;
 
 ---------------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------			
+28.FOR LOOP 
 
+
+--
+-- Seção 9 - Estruturas de Controle 
+--
+-- Aula 5 - FOR LOOP
+--
+
+-- FOR LOOP
+
+SET SERVEROUTPUT ON
+ACCEPT pLimite PROMPT 'Digite o valor do limite: '
+DECLARE
+  vInicio  INTEGER(3) := 1;
+  vFim     NUMBER(38) := &pLimite;
+BEGIN
+	FOR i IN vinicio..vfim  LOOP
+	  DBMS_OUTPUT.PUT_LINE('Número = ' || to_char(i) );
+	END LOOP;
+END;
+
+---------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------			
 
 
 
