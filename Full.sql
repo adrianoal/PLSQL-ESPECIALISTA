9685019fd8a9a3a2559b589040e2559dd67442c6
Adriano Lima <adriano.lima85@yahoo.com.br>
2020-09-03 11:18:14 -0300

    55.Recompilando Funções de Banco de Dados

--- Full.sql
+++ Full.sql
@@ -2861,6 +2861,24 @@ FROM   dual;
 
 ---------------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------			
+55.Recompilando Funções de Banco de Dados 
+
+ -- SINTAXE:
+ -----------
+ 
+ ALTER FUNCTION nome_funcao COMPILE;
+
+
+--
+-- Seção 15 - Funções de Banco de Dados
+--
+-- Aula 3 - Recompilando Funções de Banco de Dados
+--
+
+-- Recompilando Funções de Banco de Dados
+
+ALTER FUNCTION FNC_CONSULTA_SALARIO_EMPREGADO COMPILE;
+
 
 
 
