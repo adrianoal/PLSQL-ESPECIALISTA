0beb354f9571449e944e077bf20a12723c995463
Adriano Lima <adriano.lima85@yahoo.com.br>
2020-09-08 18:37:42 -0300

    Seção 20:PL/SQL 69.Database DML Triggers

--- Full.sql
+++ Full.sql
@@ -3850,8 +3850,149 @@ END PCK_EMPREGADOS;
  
 ------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------
+Seção 20:PL/SQL Fundamentos - Database DML Triggers 
+
+69.Database DML Triggers
+
+ * Desenvolva uma Database DML Trigger p/ executar um bloco PL/SQL automaticamente quando um 
+   comando de manipulação de dados (DML) for executado para uma determinada tabela.
    
+ * Triggers(gatilhos) são disparadas automaticamente quando ocorrer um evento para o qual 
+   elas foram declaradas 
    
+ * Uma database DML Trigger é associada à uma Tabela ou Visão
+
+ * Triggers podem implementar regras mais complexas de negócio para tabelas ou visões 
+
+
+ -- ALGUMAS UTILIZADADES DE UMA DATABASE TRIGGER:
+ ------------------------------------------------
+ 
+ * Manter um nível mais elevado de integridade dos dados, mais complexos que a utilização de
+   constraints.
+   
+ * Informações de auditoria de uma tabela, armazenando que informações sobre as modificações
+   foram realizados.
+   
+ * Automaticamente fazer a chamada a outros programas para realizar ações complementares
+   Uma vez criada um Trigger, ninguém consegue violar essa trigger.
+   
+ 
+ -- RESTRIÇÕES EM DATABASE DML TRIGGERS:
+ ---------------------------------------
+ 
+ * A trigger faz parte da mesma transação do comando que a disparou o Evento, então temos como
+   restrição que uma trigger não pode fazer controle de transação, ou seja, não podem executar
+   os comandos COMMIT, ROLLBACK ou SAVEPOINT a menos que seja declarado o 
+   PRAGMA AUTONOMOUS_TRANSACTIONS(Neste caso seria disparado em outra sessão, aí sim seria 
+   possível, fazer o controle da transação).
+   
+ * O corpo de um trigger não pode ser maior do que 32760 bytes, visto que o corpo de um 
+   Trigger é armazenado em uma coluna do tipo LONG.
+
+ -- ELEMENTOS DE UMA DATABASE DML TRIGGER:
+ ----------------------------------------- 
+ 
+ 
+ ELEMENTO		DESCRIÇÃO
+		
+ Tempo 			Valores Possíveis: BEFORE(antes), AFTER(depois), INSTEAD OF(ao invés de)
+ 
+ 
+ Evento			Valores Possíveis: INSERT, UPDATE[OF coluna], DELETE
+ 
+ Tipo 			Valores Possíveis: STATEMENT(Nível de comando - default)
+								   FOR EACH ROW(Nível de linha)
+								   
+ Corpo			Bloco PL/SQL 
+
+
+ * Obs: Tem 18 combinações distintas de Triggers possíveis  para uma tabela.
+ 
+  
+  Sequencia de uma Trigger se existeir todas estas para o mesmo objeto:
+  
+  BUC --> Before Update a nível de Comando
+   BUL --> Before Update a nível de Linha 
+    AUL --> After update a nível de Linha
+	 AUC --> After update a nível de Comando
+
+------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------
+Seção 20:PL/SQL 69.Database DML Triggers
+
+ * A Trigger a nível de coluna é disparado uma única vez, mesmo que o fetch manipule várias 
+   linhas.
+   
+ * Crie uma DML trigger combinando o tratamento de vários eventos, conforme infomação abaixo:
+ 
+ * Execute um tratamento específico para cada evento da trigger testando os predicados
+   INSERTING, UPDATING e DELETING para identificar o tratamento específico.
+   
+    
+ Demostração prática da Trigger nível de comando:
+ ------------------------------------------------
+ ------------------------------------------------
+ 
+--
+-- Seção 20 - Database DML Triggers
+--
+-- Aula 2 - Database DML Triggers a Nível de Comando
+--
+
+-- Database DML Triggers a Nível de Comando
+
+CREATE OR REPLACE TRIGGER B_I_EMPLOYEES_S_TRG
+BEFORE INSERT
+ON employees
+BEGIN
+  IF  (TO_CHAR(SYSDATE, 'DAY') IN ('SABADO', 'DOMINGO') OR
+       TO_NUMBER(TO_CHAR(SYSDATE,'HH24')) NOT BETWEEN 8 AND 18) 
+  THEN
+       RAISE_APPLICATION_ERROR( -20001,'O cadastramento de Empregados só é permitido em dias de semana dentro do horário comercial');
+  END IF;
+END;
+
+-- Testando a validação da Trigger
+
+BEGIN
+  PCK_EMPREGADOS.PRC_INSERE_EMPREGADO('George', 'Harrison','GHARRISON','515.258.5690',SYSDATE,'IT_PROG',25000,NULL,103,60);
+  COMMIT;
+END;
+
+-- Criando uma Trigger Combinando Vários Eventos
+
+CREATE OR REPLACE TRIGGER B_IUD_VALIDA_HORARIO_EMPLOYEES_S_TRG
+BEFORE INSERT OR UPDATE OR DELETE -- COMBINA VÁRIOS EVENTOS
+ON employees
+BEGIN
+  IF  (TO_CHAR(SYSDATE, 'DAY') IN ('SABADO', 'DOMINGO') OR
+       TO_NUMBER(TO_CHAR(SYSDATE,'HH24')) NOT BETWEEN 8 AND 18) 
+  THEN
+      CASE
+        WHEN INSERTING 
+        THEN 
+             RAISE_APPLICATION_ERROR( -20001,'O cadastramento de Empregados só é permitido em dias de semana, dentro do horário comercial');
+        WHEN DELETING
+        THEN 
+             RAISE_APPLICATION_ERROR( -20002,'A deleção de Empregados só é permitido em dias de semana, dentro do horário comercial');
+        ELSE
+             RAISE_APPLICATION_ERROR( -20003,'A atualização de Empregados só é permitido em dias de semana, dentro do horário comercial');
+        END CASE;
+  END IF;
+END;
+
+-- Testando a validação da Trigger
+
+BEGIN
+  PCK_EMPREGADOS.PRC_INSERE_EMPREGADO('George', 'Harrison','GHARRISON','515.258.5690',SYSDATE,'IT_PROG',25000,NULL,103,60);
+  COMMIT;
+END;
+
+------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------
+
+
 
 
 
