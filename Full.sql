8b4839534cd5bbfec8d20d78318c66bd0080cf92
Adriano Lima <adriano.lima85@yahoo.com.br>
2020-09-10 09:34:26 -0300

    87.Utilizando Métodos para controlar Collections

--- Full.sql
+++ Full.sql
@@ -4786,7 +4786,39 @@ END;
  Essa é a desvantagem, precisa saber quantos registros tem na tabela, pra poder usar o Varray
 ------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------  
+87.Utilizando Métodos para controlar Collections 
 
+ -- SINTAXE
+ 
+nome_collection.nomemetodo[(parametros)] -- Alguns métodos possuem parametros
+ 
+--
+-- Seção 24 - PL/SQL Avançado - Bulk Collect
+--
+-- Aula 5 - Utilizando Métodos para controlar Collections
+--
+
+-- Utilizando Métodos para controlar Collections
+
+SET SERVEROUTPUT ON
+SET VERIFY OFF
+DECLARE
+  TYPE employees_table_type IS TABLE OF employees%rowtype;
+  employees_table  employees_table_type := employees_table_type();
+BEGIN
+  SELECT *
+    BULK COLLECT INTO employees_table 
+  FROM employees;
+  FOR i IN employees_table.first..employees_table.last  
+  LOOP
+    DBMS_OUTPUT.PUT_LINE(employees_table(i).employee_id || ' - ' || 
+                         employees_table(i).first_name || ' - ' || 
+                         employees_table(i).last_name || ' - ' ||
+                         employees_table(i).phone_number || ' - ' ||
+                         employees_table(i).job_id || ' - ' ||
+                         TO_CHAR(employees_table(i).salary,'99G999G999D99'));   
+  END LOOP;
+END;
 
 
 
