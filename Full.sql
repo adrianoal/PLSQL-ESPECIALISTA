1884a364fde712f4bb8a67662ff14e6106f98c42
Adriano Lima <adriano.lima85@yahoo.com.br>
2020-09-10 09:28:57 -0300

    86.Varray of Records - Bulk Collect

--- Full.sql
+++ Full.sql
@@ -4746,13 +4746,46 @@ END;
 
 ------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------  
+86.Varray of Records - Bulk Collect 
 
+-- SINTAXE:
 
+DECLARE 
+TYPE employees_table_type IS VARRAY(200) OF employees%rowtype; 
+		-- a diferença do Nested Table p/ Varray --> Precisa definir um limite...
+employees_table employees_table_type := employees_table_type();
 
+--
+-- Seção 24 - PL/SQL Avançado - Bulk Collect
+--
+-- Aula 4 - Varray of Records - Bulk Collect
+--
 
+-- Varray of Records - Bulk Collect
 
+SET SERVEROUTPUT ON
+SET VERIFY OFF
+DECLARE
+  TYPE employees_table_type IS VARRAY(200) OF employees%rowtype; -- A tabela tem q ter menos de duzentos registro, do contrario preciso alterar o valor definido no Varray
+  employees_table  employees_table_type := employees_table_type();
+BEGIN
+  SELECT *
+    BULK COLLECT INTO employees_table 
+  FROM employees;
+  FOR i IN employees_table.first..employees_table.last  
+  LOOP
+    DBMS_OUTPUT.PUT_LINE(employees_table(i).employee_id || ' - ' || 
+                         employees_table(i).first_name || ' - ' || 
+                         employees_table(i).last_name || ' - ' ||
+                         employees_table(i).phone_number || ' - ' ||
+                         employees_table(i).job_id || ' - ' ||
+                         TO_CHAR(employees_table(i).salary,'99G999G999D99'));   
+  END LOOP;
+END;
 
-
+ Essa é a desvantagem, precisa saber quantos registros tem na tabela, pra poder usar o Varray
+------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------  
 
 
 
