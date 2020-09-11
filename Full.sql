a8a888bab4b4161c740aa27a2595aa76cd11fc7a
Adriano Lima <adriano.lima85@yahoo.com.br>
2020-09-11 12:22:10 -0300

    Seção 26: PL/SQL Avançado - SQL Dinâmico - Execute Immediate e variáveis Bind

--- Full.sql
+++ Full.sql
@@ -5034,7 +5034,101 @@ ROLLBACK;
 ------------------------------------------------------------------------------------------------  
 Seção 26: PL/SQL Avançado - SQL Dinâmico - Execute Immediate e variáveis Bind 
 
+90.SQL Dinâmico - Execute immediate
 
+ * Quando usar SQL Dinâmico?
+	Quando eu precisar executar um SQL DML em tempo de execução dentro do programa ou até 
+	mesmo um DDL.
+	
+ * Geração e execução de comandos SQL em tempo de execução
+ 
+ * Quando vc precisa executar um comando SQL DDL dentro do seu programa PL/SQL 
+ 
+ * O número ou tipo de dados de entradas e saídas é variável 
+ 
+ 
+ -- SINTAXE - EXECUTE IMMEDIATE 
+ ------------------------------
+ 
+EXECUTE IMMEDIATE 'SQL string'
+[INTO {variável[,variável]...
+	|record
+	|collection}];
+	
+	
+ -- EXEMPLO PRÁTICO:
+ -------------------
+
+--
+-- Oracle PL/SQL Avançado 
+--
+-- Seção 26 - SQL Dinâmico - Execute Immediate e variáveis Bind
+--
+-- Aula 1 - SQL Dinamico - EXECUTE IMMEDIATE
+
+-- SQL Dinamico - EXECUTE IMMEDIATE
+
+SET SERVEROUTPUT ON
+SET VERIFY OFF
+CREATE OR REPLACE PROCEDURE PRC_FETCH_EMPLOYEES_DYNAMIC (pmanager_id IN employees.manager_id%TYPE DEFAULT NULL,
+                                                         pdepartment_id IN employees.department_id%TYPE DEFAULT NULL)
+AS
+  vemployees_record  employees%ROWTYPE; -- variavel record c/ a mesma estrutura da tb employees
+  vsql               VARCHAR2(600) := 'SELECT * FROM employees WHERE 1=1 ';  -- quardando o comando seq dentro da variavel vsql
+  TYPE  employees_table_type IS TABLE OF employees%ROWTYPE   -- Associative Array de record
+  INDEX BY PLS_INTEGER;
+  employees_table            employees_table_type;
+  
+BEGIN
+
+  IF  pmanager_id IS NOT NULL THEN
+      vsql := vsql || ' AND manager_id = ' || pmanager_id;
+  END IF;
+  
+  IF  pdepartment_id IS NOT NULL THEN
+      vsql := vsql || ' AND department_id = ' || pdepartment_id;
+  END IF;
+    
+  DBMS_OUTPUT.PUT_LINE(vsql);
+  DBMS_OUTPUT.NEW_LINE;
+  
+  EXECUTE IMMEDIATE vsql -- c o retorno do meu select q esta na variavel vsql vou popular a variavel employees_table atraves do BULK COLLECT INTO
+  BULK COLLECT INTO employees_table; 
+  
+  FOR i IN 1..employees_table.COUNT  LOOP
+  
+    DBMS_OUTPUT.PUT_LINE(employees_table(i).employee_id || ' - ' ||
+                         employees_table(i).first_name || ' - ' ||
+                         employees_table(i).last_name || ' - ' ||
+                         employees_table(i).email || ' - ' ||
+                         employees_table(i).manager_id || ' - ' ||
+                         employees_table(i).department_id);
+    
+  END LOOP;
+  
+EXCEPTION
+  WHEN OTHERS THEN 
+       RAISE_APPLICATION_ERROR(-20001,'Erro Oracle ' || SQLCODE || SQLERRM);
+END;
+
+-- Executando a procedure
+
+exec PRC_FETCH_EMPLOYEES_DYNAMIC(pmanager_id => 103, pdepartment_id => 60)
+
+exec PRC_FETCH_EMPLOYEES_DYNAMIC(pmanager_id => 103)
+
+exec PRC_FETCH_EMPLOYEES_DYNAMIC(pdepartment_id => 60)
+
+exec PRC_FETCH_EMPLOYEES_DYNAMIC; 
+
+ * Só se usa o SQL Dinâmico quando precisa montar o comando em tempo de execução, do contrario
+   se usa o SQL fixo, normal.
+   A primeira regra na programação é não complicar...
+------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------  
+Seção 26: PL/SQL Avançado - SQL Dinâmico - Execute Immediate e variáveis Bind 
+
+90.SQL Dinâmico - Execute immediate
 
 
 
